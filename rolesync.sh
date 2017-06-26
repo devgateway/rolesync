bfac43cffc47149e8f8ecce03ffddd01ff3086a8
Styopa Semenukha <ssemenukha@developmentgateway.org>
2017-06-26 16:18:32 -0400

    Back up artifacts

--- rolesync.sh
+++ rolesync.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 CONFIG="${XDG_CONFIG_HOME:-$HOME/.config}/rolesync.conf"
+source "$CONFIG"
+
+# save artifacts to a local archive
+backupArtifacts() {
+	if [[ -n "$BACKUP_DIR" ]]; then
+		listArtifacts | xargs --null --no-run-if-empty tar -caf "$BACKUP_DIR/rolesync.tgz"
+	else
+		echo "Backup dir not defined, skipping backup" >&2
+	fi
+}
