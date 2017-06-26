68222c25a930e91e02f669ba493f4ab858321cc2
Styopa Semenukha <ssemenukha@developmentgateway.org>
2017-06-26 18:08:01 -0400

    Rewrote to use git instead of rsync

--- rolesync.sh
+++ rolesync.sh
@@ -1,37 +1,36 @@
 #!/bin/bash
-CONFIG="${XDG_CONFIG_HOME:-$HOME/.config}/rolesync.conf"
-source "$CONFIG"
+source "${XDG_CONFIG_HOME:-$HOME/.config}/rolesync.conf"
 
-# save artifacts to a local archive
-backupArtifacts() {
-	if [[ ! -d "$BACKUP_DIR" ]]; then
-		mkdir -p "$BACKUP_DIR"
-	fi
+case "$1" in
 
-	if [[ -n "$BACKUP_DIR" ]]; then
-		listArtifacts | xargs --null --no-run-if-empty tar -caf "$BACKUP_DIR/rolesync.tgz"
-	else
-		echo "Backup dir not defined, skipping backup" >&2
-	fi
-}
+	pack)
+		# remove controlled files from working directory
+		pushd "$SECRET_DIR" >/dev/null
+		git ls-files -z \
+			| xargs --null rm -f
 
-# list files in submodules, ignored by Git
-listArtifacts() {
-	git submodule --quiet foreach 'git ls-files --other | sed "s|^|$path/|"' | tr \\n \\0
-}
+		# copy current files from roles
+		cd "$ROLE_DIR"
+		git submodule --quiet foreach 'git ls-files --other | sed "s|^|$path/|"' \
+			| tr '\n' '\0' \
+			| rsync --verbose --files-from=- --from0 . "$SECRET_DIR"
 
-# download artifacts
-pullArtifacts() {
-}
+		popd >/dev/null
+		;;
 
-# upload artifacts
-pushArtifacts() {
-}
+	unpack)
+		pushd "$SECRET_DIR" >/dev/null
+		git archive HEAD | tar -xvC "$ROLE_DIR"
+		popd >/dev/null
+		;;
 
-(
-	pushd "$ROLE_DIR"
-
-	backupArtifacts
-
-	popd
-) >/dev/null
+	*)
+		cat >&2 <<-EOF
+		USAGE: $0 pack|unpack
+		
+		pack - gather artifacts for a new commit
+		unpack - export artifacts to roles
+		EOF
+		exit 1
+		;;
+esac
